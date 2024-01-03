940e75ae0ad34d992f4d9384f1da208906a5a439
Vanessa Arias Arias <vanessa.arias@rocketfy.co>
2024-01-03 16:02:46 -0500

    change ruta

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ const APP_SHELL = [
 const APP_SHELL_INMUTABLE = [
   'https://fonts.googleapis.com/css?family=Quicksand:300,400',
   'https://fonts.googleapis.com/css?family=Lato:400,300',
-  'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
   'css/animate.css',
   'js/libs/jquery.js'
 ];
